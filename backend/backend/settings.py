e6777f27534f7211033864f7f500c3bfa188e1c2
Анна Филатова <afficsoid@gmail.com>
2026-03-27 19:07:03 +0700

    taski deploy full

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,9 +57,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -71,9 +68,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
